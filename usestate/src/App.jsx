fb8ca7591fb2de2928385096ed5f339a37ca921a
brittLiban <britt.liban@student.greenriver.edu>
2025-04-27 17:10:41 -0700

    Done! I think?

--- usestate/src/App.jsx
+++ usestate/src/App.jsx
@@ -1,6 +1,12 @@
 import RestaurantsContainer from "./components/RestaurantsContainer";
 import { restaurants } from "./data/data.js";
 import "./App.css";
+import { useState } from "react";
+
+// ...data means to copuy over the existing useState
+// this means start restaurantState as a copy of ...data
+const [restaurantState, setRestaurants] = useState([...data])
+
 
 function App() {
   return (
